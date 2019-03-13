f5143a359bb10805a7e0a86352e725b411d63d91
Sarah Elzinga <sevans924@gmail.com>
2019-03-13 21:18:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 };
 
 function accessElementInArray(array, idex){
-  console.log(array[index]);
+  return array[index];
 };
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
