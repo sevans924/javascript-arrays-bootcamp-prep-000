920c250ee41b2b40e2f55111448b47e680429291
Sarah Elzinga <sevans924@gmail.com>
2019-03-13 21:19:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,8 +32,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 };
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
-  return array
+  return array.slice(1);
+  
 };
 
 function destructivelyRemoveElementFromEndOfArray(array){
